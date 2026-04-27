5f04678eced383f4ac6401a4d4aed340c4828246
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2026-04-28 02:42:41 +0800

    feat: allow access with superuser

--- accounts/mixins.py
+++ accounts/mixins.py
@@ -9,6 +9,9 @@ class RoleRequiredMixin(AccessMixin):
         if not request.user.is_authenticated:
             return self.handle_no_permission()
 
+        if request.user.is_superuser:
+            return super().dispatch(request, *args, **kwargs)
+
         if not hasattr(request.user, 'profile') or request.user.profile.role != self.required_role:
             raise PermissionDenied(
                 "You do not have the required role to access this page.")
