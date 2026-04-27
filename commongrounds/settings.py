75b008834ce05d038d136357de5f6e20d04e92c8
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2026-04-28 02:04:33 +0800

    feat: create templates

--- commongrounds/settings.py
+++ commongrounds/settings.py
@@ -132,7 +132,7 @@ STATIC_ROOT = os.getenv('STATIC_ROOT')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 LOGIN_URL = "/accounts/login/"
-LOGIN_REDIRECT_URL = "/"
-LOGOUT_REDIRECT_URL = "/"
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
