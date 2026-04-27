6f4f13ca5b0e6734e342fc6d885ab5d900dea0a0
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2026-04-28 01:09:33 +0800

    chore(accounts): register app and auth settings

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    ROLE_CHOICES = [
+        ("Market Seller", "Market Seller"),
+        ("Event Organizer", "Event Organizer"),
+        ("Book Contributor", "Book Contributor"),
+        ("Project Creator", "Project Creator"),
+        ("Commission Maker", "Commission Maker"),
+    ]
+
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name="profile")
+    display_name = models.CharField(max_length=63)
+    email_address = models.EmailField()
+    role = models.CharField(max_length=63, choices=ROLE_CHOICES, blank=True)
+
+    def __str__(self):
+        return f"{self.display_name} ({self.user})"
