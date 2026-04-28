29c7d87e197dd76991fcc40c0fcef6c0440c5d51
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2026-04-28 20:22:10 +0800

    feat: update html files for better ui, update configs for dev images

--- commongrounds/settings.py
+++ commongrounds/settings.py
@@ -131,6 +131,9 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.getenv('STATIC_ROOT')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 LOGIN_URL = "/accounts/login/"
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
