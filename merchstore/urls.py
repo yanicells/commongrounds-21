47c5069e039f370a43642b7dfb171ef0aed53840
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2026-04-28 19:50:06 +0800

    feat: map merchstore views to final project spec URLs

--- merchstore/urls.py
+++ merchstore/urls.py
@@ -7,4 +7,9 @@ app_name = 'merchstore'
 urlpatterns = [
     path('items', views.ItemListView.as_view(), name='item-list'),
     path('item/<int:pk>', views.ItemDetailView.as_view(), name='item-detail'),
+    path('item/add', views.ItemCreateView.as_view(), name='item-add'),
+    path('item/<int:pk>/edit', views.item_update_view, name='item-update'),
+    path('cart', views.CartView.as_view(), name='cart'),
+    path('transactions', views.TransactionListView.as_view(),
+         name='transaction-list'),
 ]
