da3be3801ee6fdfee21cf6126fa468208eb1c117
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2026-04-28 19:31:29 +0800

    feat: create product and transaction forms

---
+++ merchstore/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Product, Transaction
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = [
+            'name', 'product_type', 'product_image',
+            'description', 'price', 'stock', 'status'
+        ]
+
+    class TransactionForm(forms.ModelForm):
+        class Meta:
+            model = Transaction
+            fields = ['amount']
