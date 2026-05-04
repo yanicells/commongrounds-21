6f294c5af31ae005c8925062ceffa22dc9b05f83
jktrr <jkterrobias@gmail.com>
2026-05-04 09:08:22 +0800

    fix: merge

--- commissions/urls.py
+++ commissions/urls.py
@@ -6,14 +6,7 @@ from . import views
 app_name = 'commissions'
 
 urlpatterns = [
-<<<<<<< HEAD
     path('requests', views.RequestListView.as_view(), name='request-list'),
     path('request/<int:pk>', views.RequestDetailView.as_view(),
          name='request-detail'),
 ]
-=======
-    path('admin/', admin.site.urls),
-    path('requests', CommissionListView.as_view(), name='list_view'),
-    path('request/<int:pk>', CommissionDetailView.as_view(), name='detail_view')
-]
->>>>>>> 9693708 (fix: urls and missing model field)
