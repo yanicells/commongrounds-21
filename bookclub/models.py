772f142db314c1fcdfcf3f6a6b3e9b4016357e5a
PJ <edizon.luis.infante@student.ateneo.edu>
2026-05-05 01:25:42 +0800

    feat: add new model fields and make migrations + update urls.py

--- bookclub/models.py
+++ bookclub/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from accounts.models import Profile
 
 
 class Genre(models.Model):
@@ -21,8 +22,16 @@ class Book(models.Model):
         null=True,
         blank=True,
     )
+    contributor = models.ForeignKey(
+        Profile,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
     author = models.CharField(max_length=255)
+    synopsis = models.TextField()
     publication_year = models.IntegerField()
+    available_to_borrow = models.BooleanField(default=True)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
@@ -34,3 +43,43 @@ class Book(models.Model):
 
     def get_absolute_url(self):
         return reverse("bookclub:book-detail", args=[str(self.pk)])
+
+
+class BookReview(models.Model):
+    user_reviewer = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+    )
+    anon_reviewer = models.CharField(max_length=255, blank=True, null=True)
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    comment = models.TextField()
+
+
+class Bookmark(models.Model):
+    profile = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+    )
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+    )
+    date_bookmarked = models.DateTimeField(auto_now_add=True)
+
+
+class Borrow(models.Model):
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+    )
+    borrower = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+    )
+    name = models.CharField(max_length=255)
+    date_borrowed = models.DateTimeField(auto_now_add=True)
+    date_to_return = models.DateTimeField()
