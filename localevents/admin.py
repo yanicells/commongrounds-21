4e2135ece3b86f01c2fc0aa4843e0b4d5781cfd5
Nathan Martin <gabryllmartin@gmail.com>
2026-05-05 19:49:51 +0800

    feat: registered EventSignup model in admin

--- localevents/admin.py
+++ localevents/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Event, EventType
+from .models import Event, EventType, EventSignup
 
 
 class EventTypeAdmin(admin.ModelAdmin):
@@ -11,5 +11,10 @@ class EventAdmin(admin.ModelAdmin):
     model = Event
 
 
+class EventSignupAdmin(admin.ModelAdmin):
+    model = EventSignup
+
+
 admin.site.register(EventType, EventTypeAdmin)
 admin.site.register(Event, EventAdmin)
+admin.site.register(EventSignup, EventSignupAdmin)
