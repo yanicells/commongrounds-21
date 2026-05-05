58ddeaa0820e972565a73b05ca540adf00ae735d
Eostrixx <sofia.dion.torres@student.ateneo.edu>
2026-05-05 22:54:43 +0800

    feat: register Favorite, ProjectRating, and ProjectReview in admin

--- diyprojects/admin.py
+++ diyprojects/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Project, ProjectCategory
+from .models import Project, ProjectCategory, Favorite, ProjectRating, ProjectReview
 
 
 class ProjectCategoryAdmin(admin.ModelAdmin):
@@ -10,6 +10,18 @@ class ProjectCategoryAdmin(admin.ModelAdmin):
 class ProjectAdmin(admin.ModelAdmin):
     model = Project
 
+class FavoriteAdmin(admin.ModelAdmin):
+    model = Favorite
+
+class ProjectRatingAdmin(admin.ModelAdmin):
+    model = ProjectRating
+
+class ProjectReviewAdmin(admin.ModelAdmin):
+    model = ProjectReview
 
 admin.site.register(ProjectCategory, ProjectCategoryAdmin)
 admin.site.register(Project, ProjectAdmin)
+admin.site.register(Favorite, FavoriteAdmin)
+admin.site.register(ProjectRating, ProjectRatingAdmin)
+admin.site.register(ProjectReview, ProjectReviewAdmin)
+
