c9daea22b018d2cc6531f500c9dd4cfd0e871451
PJ <edizon.luis.infante@student.ateneo.edu>
2026-05-06 03:09:59 +0800

    feat: Add book review, bookmark, and borrow models and admin registration + forms + settings/url update

--- commongrounds/settings.py
+++ commongrounds/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 CSRF_TRUSTED_ORIGINS = [f'https://{host}' for host in ALLOWED_HOSTS if host]
 
