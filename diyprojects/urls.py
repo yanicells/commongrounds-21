aa476bf41f09569f9680252f98ca70471b32bbdd
Eostrixx <sofia.dion.torres@student.ateneo.edu>
2026-05-06 00:11:19 +0800

    feat: fixed errors and updated html files for ui

--- diyprojects/urls.py
+++ diyprojects/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('project/<int:pk>', views.ProjectDetailView.as_view(),
          name='project-detail'),
     path('project/add', views.ProjectCreateView.as_view(), name='project-add'),
-    path('project/<int:pk>/edit', views.project_update_view, name='item-update')
+    path('project/<int:pk>/edit', views.project_update_view, name='project-update')
 ]
