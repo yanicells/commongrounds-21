d35738ab6d140a9434e0b844d18663441739c619
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2026-05-07 00:55:17 +0800

    feat: add comment functionality, migrate

--- grounds/admin.py
+++ grounds/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 class PostAdmin(admin.ModelAdmin):
     model = Post
 
 
+class CommentAdmin(admin.ModelAdmin):
+    model = Comment
+
+
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
--- grounds/models.py
+++ grounds/models.py
@@ -15,3 +15,15 @@ class Post(models.Model):
 
     def __str__(self):
         return str(self.title)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.post}'
