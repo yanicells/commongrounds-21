6653e6c8a039557eba0e8e6b5c8577b89cb70c85
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2026-05-07 00:55:17 +0800

    feat: add comment functionality, migrate

--- grounds/views.py
+++ grounds/views.py
@@ -1,14 +1,41 @@
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
-from .models import Post
-from .forms import PostForm
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
 
 class PostListView(ListView):
     model = Post
     template_name = 'grounds/post_list.html'
     context_object_name = 'posts'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'grounds/post_detail.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = CommentForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('login')
+        
+        self.object = self.get_object()
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = self.object
+            comment.author = request.user.profile
+            comment.save()
+            return redirect('grounds:post-detail', pk=self.object.pk)
+        
+        context = self.get_context_data()
+        context['form'] = form
+        return self.render_to_response(context)
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
