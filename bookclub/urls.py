7c4d2ffd342d4c65293856cde107f5cd090d99b4
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2026-05-06 21:20:38 +0800

    feat: implement shared layout

--- bookclub/urls.py
+++ bookclub/urls.py
@@ -5,9 +5,9 @@ from . import views
 app_name = 'bookclub'
 
 urlpatterns = [
-    path('books/', views.BookListView.as_view(), name='book-list'),
-    path('book/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
-    path('book/add/', views.BookCreateView.as_view(), name='book-create'),
-    path('book/<int:pk>/borrow/', views.BookBorrowView.as_view(), name='book-borrow'),
-    path('book/<int:pk>/edit/', views.book_update_view, name='book-update'),
+    path('books', views.BookListView.as_view(), name='book-list'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('book/add', views.BookCreateView.as_view(), name='book-create'),
+    path('book/<int:pk>/borrow', views.BookBorrowView.as_view(), name='book-borrow'),
+    path('book/<int:pk>/edit', views.book_update_view, name='book-update'),
 ]
