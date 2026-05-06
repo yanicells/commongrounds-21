3f3d81fc051af929fc66a20f639a633e7cddd121
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2026-05-06 20:26:41 +0800

    feat: fix migrations and update role checks

--- commissions/migrations/0004_commission_maker_commission_status_job_and_more.py
+++ commissions/migrations/0004_commission_maker_commission_status_job_and_more.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accounts', '0002_alter_profile_id'),
+        ('accounts', '0001_initial'),
         ('commissions', '0003_alter_commission_people_required'),
     ]
 
