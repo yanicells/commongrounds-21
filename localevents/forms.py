cd57bcf0bcdd2d10606bb9b34313861a796883d9
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2026-05-06 20:49:07 +0800

    feat: update category logic

--- localevents/forms.py
+++ localevents/forms.py
@@ -4,7 +4,7 @@ from .models import Event, EventSignup
 class EventForm(forms.ModelForm): 
     class Meta: 
         model = Event 
-        exclude = ['organizer', 'category']
+        exclude = ['organizer']
     
 class EventSignupForm(forms.ModelForm):
     class Meta:
